4b19792a9153165461985989cb9282aca7c37cf5
EnterTheNameHere Bohemian <email+github@enterthenamehere.com>
2025-07-26 10:16:39 +0200

    feat(electron): enable contextIsolation and devtools in debug mode

--- electron/main.js
+++ electron/main.js
@@ -1,16 +1,24 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+globalThis.__turnixDevMode = true;
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
-            preload: path.join(__dirname, "preload.js")
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true,
+            nodeIntegration: false,
         },
     });
 
     win.loadURL("http://localhost:3000/")
+
+    if(globalThis.__turnixDevMode) {
+        win.webContents.openDevTools()
+    }
 }
 
 app.whenReady().then(() => {
