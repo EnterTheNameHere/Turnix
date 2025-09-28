882fe9c0861c68a2eb02b6794e5abc994a523ba8
EnterTheNameHere Bohemian <email+github@enterthenamehere.com>
2025-09-28 09:32:45 +0200

    feat(launcher): add title to launcher's terminal window

--- launcher.bat
+++ launcher.bat
@@ -1,1 +1,1 @@
-pwsh.exe -ExecutionPolicy ByPass -NoExit -Command "Conda-Environment Turnix; python ./launcher.py"
+pwsh.exe -ExecutionPolicy ByPass -NoExit -Command "$host.UI.RawUI.WindowTitle = 'Turnix Launcher'; python-embedded/python.exe ./launcher.py"
