de76e810f190206ff985740063a6130902903e19
EnterTheNameHere Bohemian <email+github@enterthenamehere.com>
2025-11-05 16:06:27 +0100

    feat(server): use basic setup for logging before config system is initialized

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,11 @@
 # backend/server.py
 from __future__ import annotations
+import logging
+
+# Basic logging setup, before we can initialize config service to read logging config
+logging.basicConfig(level=logging.INFO) # Set to INFO temporarily
+logger = logging.getLogger(__name__)
+logger.info("Basic logging initiated...")
 
 from backend.app.factory import createApp
 
