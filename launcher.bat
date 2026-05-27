18e6c084209c673adbff6d5118de4ef9f73d2a3e
EnterTheNameHere Bohemian <email+github@enterthenamehere.com>
2026-05-27 03:02:30 +0200

    fix(launcher): correctly chain launcher title and embedded Python command

--- launcher.bat
+++ launcher.bat
@@ -1,1 +1,1 @@
-pwsh.exe -ExecutionPolicy ByPass -NoExit -Command "$host.UI.RawUI.WindowTitle = 'Turnix Launcher'; python-embedded/python.exe ./launcher.py"
+pwsh.exe -ExecutionPolicy ByPass -NoExit -Command "$host.UI.RawUI.WindowTitle = 'Turnix Launcher' & python-embedded/python.exe ./launcher.py"
