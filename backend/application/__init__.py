0d329e8b3d49a86277e8f854e91a08b089113039
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2026-09-07 05:01:24 +0200

    Export Application lifecycle coordinator

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,3 +1,10 @@
+# file: backend/application/__init__.py ; version: 1
+from backend.application.lifecycle import ApplicationLifecycle
 from backend.application.runtime import Application, ApplicationRun, ApplicationRunState
 
-__all__ = ["Application", "ApplicationRun", "ApplicationRunState"]
+__all__ = [
+    "Application",
+    "ApplicationLifecycle",
+    "ApplicationRun",
+    "ApplicationRunState",
+]
